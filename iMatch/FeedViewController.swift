f904e9f1ad4188c666e277e9bd282f25895d5049
Iwan Siauw <evansiauw@gmail.com>
2018-08-20 20:17:09 -0400

    Added FeedViewController for adding arbritrary table cell view

---
+++ iMatch/FeedViewController.swift
@@ -0,0 +1,94 @@
+//
+//  FeedViewController.swift
+//  Pods-iMatch
+//
+//  Created by Iwan Siauw on 8/19/18.
+//
+
+import UIKit
+
+var feedArray = ["Room For Rent","Studio for sale in Elmhurst","Free Sofa"];
+var feedArrayDetails = [
+    "1 bedroom in 3 bedroom apt is available in Elmhurst\nPlease Call 347-933-2366",
+    "Large Bedroom Alcove Area. Very Low Maintenance.\nExtremely Convenient Location",
+    "First come first save.\nPick up by today at 42-54 81st street apt 4"
+];
+
+
+class FeedViewController: UITableViewController{
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return feedArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FeedTableViewCell
+        
+        cell.feedImage.image = UIImage(named: "hamster.jpg");
+        cell.feedTitle.text = feedArray[indexPath.row];
+        cell.feedSubTitle.text = feedArrayDetails[indexPath.row];
+    
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
